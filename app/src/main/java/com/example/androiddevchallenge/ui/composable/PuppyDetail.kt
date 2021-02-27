c06aac29f1250015f627fc7e52827613e3789bbd
ntsk <ridha.h2a26@gmail.com>
2021-02-27 16:49:33 +0900

    Fix spotless verification

--- app/src/main/java/com/example/androiddevchallenge/ui/composable/PuppyDetail.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/composable/PuppyDetail.kt
@@ -1,9 +1,35 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.composable
 
 import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Phone
@@ -37,8 +63,7 @@ fun PuppyDetailTopAppBar(puppyName: String, onBackButtonClick: () -> Unit) {
     TopAppBar(
         title = { Text(puppyName) },
         navigationIcon = {
-            IconButton(onClick = { onBackButtonClick() })
-            { Icon(Icons.Filled.ArrowBack, contentDescription = "Back") }
+            IconButton(onClick = { onBackButtonClick() }) { Icon(Icons.Filled.ArrowBack, contentDescription = "Back") }
         },
     )
 }
